116bedc82de966e6699414d9e569330165e86aa2
Stallix <thestallix@gmail.com>
2019-06-21 11:33:35 +0530

    n

--- aosp_lavender.mk
+++ aosp_lavender.mk
@@ -13,7 +13,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 $(call inherit-product, device/xiaomi/lavender/device.mk)
 
 # Inherit some common Syberia stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 #TARGET_BOOT_ANIMATION_RES_EVO := true
 #CUSTOM_BUILD_TYPE := OFFICIAL
 TARGET_GAPPS_ARCH := arm64
